a1777edf1fca61b6f55d35e1ff726f4d919ee505
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-29 23:24:22 -0400

    chore: added code block categorization and removed unnecessary spacing across server side code

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,8 @@
+// Imports
 const db = require('./db')
 
 // Register models
 require('./models')
 
+// Exports
 module.exports = db
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,3 +1,4 @@
+// Exports
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)

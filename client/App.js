9dca4bcd96823c62f5522710b9e0ef49ed8731f9
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-30 05:02:35 -0400

    feat: recreated the App and Routes components

---
+++ client/App.js
@@ -0,0 +1,18 @@
+// Imports
+import React from 'react'
+
+import {Navbar} from './components'
+import Routes from './Routes'
+
+// Component
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <Routes />
+    </div>
+  )
+}
+
+export default App
---
+++ client/Routes.js
@@ -0,0 +1,58 @@
+// Imports
+import React, {useEffect} from 'react'
+import {withRouter, Route, Switch} from 'react-router-dom'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+
+import {Login, Signup, Home, PageNotFound} from './components'
+import {me} from './store'
+
+// Component
+const Routes = ({loadInitialData, isLoggedIn}) => {
+  useEffect(
+    () => {
+      loadInitialData()
+    },
+    [loadInitialData]
+  )
+
+  return (
+    <Switch>
+      {/* Routes placed here are available to all visitors */}
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      {isLoggedIn && (
+        <Switch>
+          {/* Routes placed here are only available after logging in */}
+          <Route path="/home" component={Home} />
+          <Route path="/:wildcard" component={PageNotFound} />
+        </Switch>
+      )}
+      {/* Displays our Login component as a fallback */}
+      <Route component={Login} />
+    </Switch>
+  )
+}
+
+// Container
+const mapStateToProps = state => ({
+  // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+  // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+  isLoggedIn: !!state.user.id
+})
+
+const mapDispatchToProps = dispatch => ({
+  loadInitialData() {
+    dispatch(me())
+  }
+})
+
+// The `withRouter` wrapper makes sure that updates are not blocked
+// when the url changes
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Routes))
+
+// Prop Types
+Routes.propTypes = {
+  loadInitialData: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
+}

4ae3d8e514c9f76da5e5da0d54decaa83d3a65bd
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-30 03:40:51 -0400

    feat: refactored the AuthForm component to support first and last name inputs and overall field validation

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,43 +1,155 @@
 // Imports
-import React from 'react'
+import React, {Fragment, useState} from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 
 import {auth} from '../store'
 
 // Component
-const AuthForm = ({name, displayName, error, handleSubmit}) => {
+const AuthForm = ({name, displayName, error, authThunk}) => {
+  const [passwordValidationError, setPasswordValidationError] = useState(false)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    const formName = event.target.name
+    const email = event.target.email.value
+    const password = event.target.password.value
+    const passwordValidation = event.target.passwordValidation
+      ? event.target.passwordValidation.value
+      : null
+    const firstName = event.target.firstName
+      ? event.target.firstName.value
+      : null
+    const lastName = event.target.lastName ? event.target.lastName.value : null
+
+    if (
+      formName === 'login' ||
+      (formName === 'signup' && password === passwordValidation)
+    ) {
+      setPasswordValidationError(false)
+      authThunk(formName, email, password, firstName, lastName)
+    } else {
+      setPasswordValidationError(true)
+    }
+  }
+
   return (
     <div className="center">
+      <h4>{displayName}</h4>
+
       <form
-        className="auth-container center"
+        className="center auth-form-container"
         name={name}
         onSubmit={handleSubmit}
       >
-        <div className="auth-containee">
+        <div className="form-containee">
           <label htmlFor="email">
-            <small>Email</small>
+            <span>Email</span>
+
+            <span className="text-color-red">*</span>
           </label>
 
-          <input name="email" autoComplete="email" type="text" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            autoComplete="email"
+            required
+          />
         </div>
 
-        <div className="auth-containee">
+        <div className="form-containee">
           <label htmlFor="password">
-            <small>Password</small>
+            <span>Password</span>
+
+            <span className="text-color-red">*</span>
           </label>
 
-          <input name="password" autoComplete="password" type="password" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            minLength="3"
+            autoComplete="password"
+            required
+          />
         </div>
 
-        <div className="auth-containee">
-          <button type="submit">{displayName}</button>
+        {name === 'signup' ? (
+          <Fragment>
+            <div className="form-containee">
+              <label htmlFor="passwordValidation">
+                <span>Password Validation</span>
+
+                <span className="text-color-red">*</span>
+              </label>
+
+              <input
+                type="password"
+                name="passwordValidation"
+                placeholder="Re-enter Password"
+                minLength="3"
+                autoComplete="password-validation"
+                required
+              />
+            </div>
+
+            <div className="form-containee">
+              <label htmlFor="firstName">
+                <span>First Name</span>
+
+                <span className="text-color-red">*</span>
+              </label>
+
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Enter First Name"
+                autoComplete="first-name"
+                required
+              />
+            </div>
+
+            <div className="form-containee">
+              <label htmlFor="lastName">
+                <span>Last Name</span>
+
+                <span className="text-color-red">*</span>
+              </label>
+
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Enter Last Name"
+                autoComplete="last-name"
+                required
+              />
+            </div>
+          </Fragment>
+        ) : null}
+
+        <div className="form-button-containee">
+          <button className="btn" type="submit">
+            <span>{displayName}</span>
+          </button>
         </div>
 
-        {error && error.response && <div> {error.response.data} </div>}
+        {error &&
+          error.response && (
+            <div className="text-color-red">{`Error! ${
+              error.response.data
+            }.`}</div>
+          )}
+
+        {passwordValidationError && (
+          <div className="text-color-red">
+            Error! Please re-enter your password.
+          </div>
+        )}
       </form>
 
-      <div className="center">
+      <div className="center google-oauth">
         <a href="/auth/google">{displayName} with Google</a>
       </div>
     </div>
@@ -51,35 +163,23 @@ const AuthForm = ({name, displayName, error, handleSubmit}) => {
  *   function, and share the same Component. This is a good example of how we
  *   can stay DRY with interfaces that are very similar to each other!
  **/
-const mapLoginToProps = state => {
-  return {
-    name: 'login',
-    displayName: 'Login',
-    error: state.user.error
+const mapLoginToProps = state => ({
+  name: 'login',
+  displayName: 'Log In',
+  error: state.user.error
+})
+
+const mapSignupToProps = state => ({
+  name: 'signup',
+  displayName: 'Sign Up',
+  error: state.user.error
+})
+
+const mapDispatchToProps = dispatch => ({
+  authThunk(formName, email, password, firstName, lastName) {
+    dispatch(auth(formName, email, password, firstName, lastName))
   }
-}
-
-const mapSignupToProps = state => {
-  return {
-    name: 'signup',
-    displayName: 'Sign Up',
-    error: state.user.error
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    handleSubmit(event) {
-      event.preventDefault()
-
-      const formName = event.target.name
-      const email = event.target.email.value
-      const password = event.target.password.value
-
-      dispatch(auth(email, password, formName))
-    }
-  }
-}
+})
 
 export const Login = connect(mapLoginToProps, mapDispatchToProps)(AuthForm)
 export const Signup = connect(mapSignupToProps, mapDispatchToProps)(AuthForm)
@@ -89,5 +189,5 @@ AuthForm.propTypes = {
   name: PropTypes.string.isRequired,
   displayName: PropTypes.string.isRequired,
   error: PropTypes.object,
-  handleSubmit: PropTypes.func.isRequired
+  authThunk: PropTypes.func.isRequired
 }

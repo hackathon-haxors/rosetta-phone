9b4b6eaab775c2a64e1b88c6e76fafe8e1ad366e
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-30 03:46:25 -0400

    feat: created layoutReducer to support toggling the Preloader component on and off and plugged it into the root reducer

--- client/store/index.js
+++ client/store/index.js
@@ -1,15 +1,22 @@
+// Imports
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
-import {userReducer} from './userReducer'
+import userReducer from './userReducer'
+import layoutReducer from './layoutReducer'
 
-const reducer = combineReducers({user: userReducer})
+// Initializations
+const reducer = combineReducers({
+  user: userReducer,
+  layout: layoutReducer
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
 const store = createStore(reducer, middleware)
 
+// Exports
 export default store
 export * from './userReducer'

85c838f765735b25a28d7157c99394d2c1ab4546
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-07 23:56:42 -0400

    chore: changed default socket logs to improve messaging on socket connects and disconnects on both server side and client side

--- client/socket.js
+++ client/socket.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client'
 const socket = io(window.location.origin)
 
 socket.on('connect', () => {
-  console.log('Connected!')
+  console.log('Socket connected!')
 })
 
 // Exports

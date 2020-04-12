78164b1ce1a635a134524148b170746a9d1ceac3
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-11 23:49:50 -0400

    feat: created burger menu styles using the app color palette

---
+++ client/styles.js
@@ -0,0 +1,48 @@
+// Initializations
+const burgerStyles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    right: '14px',
+    top: '14px'
+  },
+  bmBurgerBars: {
+    background: '#f64060'
+  },
+  bmBurgerBarsHover: {
+    background: '#a90000'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#fafafa'
+  },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100%'
+  },
+  bmMenu: {
+    background: '#f64060',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    backgroundColor: '#f64060',
+    padding: '0.8em'
+  },
+  bmItem: {
+    display: 'inline-block'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
+
+// Exports
+export default {burgerStyles}

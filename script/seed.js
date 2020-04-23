e71a64b40e7e1bbfb6a7bc0a8a317c550c88a012
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-23 00:03:20 -0400

    feat: added googleId, fullName, and imgUrl to instances created in the seed file

--- script/seed.js
+++ script/seed.js
@@ -15,16 +15,22 @@ async function seed() {
 
   const users = await Promise.all([
     User.create({
+      googleId: '111111111111111111111',
+      email: 'cody@puppybook.com',
       firstName: 'Cody',
       lastName: 'DaPug',
-      email: 'cody@email.com',
-      password: '123'
+      fullName: 'Cody DaPug',
+      imgUrl:
+        'https://vetstreet.brightspotcdn.com/dims4/default/354d0cf/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fdc%2Fc4%2F8ccd3a28438d81b2f2f5d8031a05%2Fpug-ap-r82p3q-645.jpg'
     }),
     User.create({
+      googleId: '222222222222222222222',
+      email: 'murphy@puppybook.com',
       firstName: 'Murphy',
       lastName: 'DaPug',
-      email: 'murphy@email.com',
-      password: '123'
+      fullName: 'Cody DaPug',
+      imgUrl:
+        'https://vetstreet.brightspotcdn.com/dims4/default/354d0cf/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fdc%2Fc4%2F8ccd3a28438d81b2f2f5d8031a05%2Fpug-ap-r82p3q-645.jpg'
     })
   ])
 

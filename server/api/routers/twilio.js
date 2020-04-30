64c6403b0fafea2546d38dbc07f022ce34426b67
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-30 16:59:13 -0400

    feat: refactored the /sms route in the twilio router to allow initial text messages to be sent from the TextForm component

--- server/api/routers/twilio.js
+++ server/api/routers/twilio.js
@@ -7,40 +7,75 @@ const client = require('twilio')(
 const {MessagingResponse} = require('twilio').twiml
 const {Translate} = require('@google-cloud/translate').v2
 
+// Models
+const {User} = require('../../db/models')
+
 // Initializations
-const sendSms = body => {
+const bodyRegex = /[^\w\s,.!?'"]/gi
+const toRegex = /-+/g
+
+const sendSms = (body, to) => {
   const payload = {
     body,
     from: process.env.PN_TWILIO,
-    to: process.env.PN_TEST
+    to
   }
+
   console.log({payload})
 
   client.messages.create(payload).then(message => console.log(message.sid))
 }
 
+// Middleware
+const sendTextAuth = (req, res, next) => {
+  if (req.user.role === 'Doctor') {
+    next()
+  } else {
+    res.status(401).send('Access Denied')
+  }
+}
+
 // Routes
-router.post('/sms', async (req, res, next) => {
-  const {Body, From, To} = req.body
+router.post('/sms', sendTextAuth, async (req, res, next) => {
+  let {googleId, text, Body, From, To} = req.body
+
+  console.log({googleId, text, Body, From, To})
 
   try {
     const twiml = new MessagingResponse()
     const translate = new Translate()
 
+    let patient
+    if (googleId) {
+      patient = await User.findOne({
+        where: {googleId},
+        // Explicitly select only the fullName, language, and phone fields -
+        // even though users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ['fullName', 'language', 'phone']
+      })
+
+      // // Manually set patient phone number for testing purposes
+      // patient.phone = process.env.PN_TEST
+
+      console.log({patient})
+
+      if (patient && patient.fullName && patient.phone) {
+        Body = `Hello, ${patient.fullName}. ${text.replace(bodyRegex, '')}`
+        To = `+1${patient.phone.replace(toRegex, '')}`
+      }
+    }
+
     // Print sender and message
     console.log(`Incoming text message from ${From} to ${To}: ${Body}`)
 
-    // // Translate message to target language
-    // const target = 'es'
-    // const [result, _] = await translate.translate(Body, target)
+    // // Translate message to patient language
+    // const [result, _] = await translate.translate(Body, patient.language || 'en')
     // console.log({result})
+    // Body = result
 
-    // Temporary result variable for testing purposes
-    const result = Body
-    console.log({result})
-
-    sendSms(result)
-    twiml.message(result)
+    sendSms(Body, To)
+    twiml.message(Body)
 
     res.writeHead(200, {'Content-Type': 'text/xml'})
     res.end(twiml.toString())

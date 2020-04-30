b4935535df0ca12b1d1377d1a8ea2ceaec178fb0
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-30 11:58:11 -0400

    feat: updated RESTful GET route to retrieve the role, language, and phone properties from the database, created a RESTful PUT route to update initial user data with role, language, and phone values, and created security middleware for both routes to only allow access to authorized users in the users router

--- server/api/routers/users.js
+++ server/api/routers/users.js
@@ -4,16 +4,37 @@ const router = require('express').Router()
 // Models
 const {User} = require('../../db/models')
 
+// Middleware
+const readUserAuth = (req, res, next) => {
+  if (
+    (req.user.googleId === req.params.googleId && !req.user.completedSignup) ||
+    req.user.role === 'Doctor'
+  ) {
+    next()
+  } else {
+    res.status(401).send('Access Denied')
+  }
+}
+
+const updateUserAuth = (req, res, next) => {
+  if (req.user.googleId === req.params.googleId) {
+    next()
+  } else {
+    res.status(401).send('Access Denied')
+  }
+}
+
 // Routes
-router.get('/:googleId', async (req, res, next) => {
+router.get('/:googleId', readUserAuth, async (req, res, next) => {
   const {googleId} = req.params
 
   try {
     const user = await User.findOne({
       where: {googleId},
       // Explicitly select only the id, googleId, email, firstName, lastName,
-      // fullName, and imgUrl fields - even though users' passwords are
-      // encrypted, it won't help if we just send everything to anyone who asks!
+      // fullName, imgUrl, role, language, phone, and completedSignup fields -
+      // even though users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
       attributes: [
         'id',
         'googleId',
@@ -21,11 +42,40 @@ router.get('/:googleId', async (req, res, next) => {
         'firstName',
         'lastName',
         'fullName',
-        'imgUrl'
+        'imgUrl',
+        'role',
+        'language',
+        'phone',
+        'completedSignup'
       ]
     })
 
     res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:googleId', updateUserAuth, async (req, res, next) => {
+  const {googleId} = req.params
+  const {role, language, phone} = req.body
+
+  try {
+    const [_, updatedUser] = await User.update(
+      {
+        role,
+        language,
+        phone,
+        completedSignup: true
+      },
+      {
+        where: {googleId},
+        returning: true,
+        plain: true
+      }
+    )
+
+    res.json(updatedUser)
   } catch (error) {
     next(error)
   }

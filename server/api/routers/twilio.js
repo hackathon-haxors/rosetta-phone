2e530a58eae80af4385018c306908a9acfdbc410
Tal Luigi <luigi.t.agr@gmail.com>
2021-05-08 23:05:52 -0400

    chore: rolled twilio.js back to pre code breaking state

--- server/api/routers/twilio.js
+++ server/api/routers/twilio.js
@@ -1,19 +1,11 @@
 // Imports
 const router = require('express').Router()
-const twilioClient = require('twilio')(
+const client = require('twilio')(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
 )
 const {MessagingResponse} = require('twilio').twiml
 const {Translate} = require('@google-cloud/translate').v2
-const {auth} = require('google-auth-library')
-
-// Load the environment variable with our keys
-const keysEnvVar = process.env.GCP_CRED
-if (!keysEnvVar) {
-  throw new Error('The $GCP_CRED environment variable was not found!')
-}
-const keys = JSON.parse(keysEnvVar)
 
 // Models
 const {User} = require('../../db/models')
@@ -21,19 +13,14 @@ const {User} = require('../../db/models')
 // Initializations
 const bodyRegex = /[^\w\s,.!?'"]/gi
 const toRegex = /-+/g
-
 const sendSms = (body, to) => {
   const payload = {
     body,
     from: process.env.PN_TWILIO,
     to
   }
-
   console.log({payload})
-
-  twilioClient.messages
-    .create(payload)
-    .then(message => console.log(message.sid))
+  client.messages.create(payload).then(message => console.log(message.sid))
 }
 
 // Middleware
@@ -48,20 +35,10 @@ const sendTextAuth = (req, res, next) => {
 // Routes
 router.post('/sms', sendTextAuth, async (req, res, next) => {
   let {googleId, text, Body, From, To} = req.body
-
   console.log({googleId, text, Body, From, To})
-
   try {
-    // Load the JWT or UserRefreshClient from the keys
-    const client = auth.fromJSON(keys)
-    client.scopes = ['https://www.googleapis.com/auth/cloud-platform']
-    const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`
-    const response = await client.request({url})
-    console.log(response.data)
-
     const twiml = new MessagingResponse()
     const translate = new Translate()
-
     let patient
     if (googleId) {
       patient = await User.findOne({
@@ -73,8 +50,8 @@ router.post('/sms', sendTextAuth, async (req, res, next) => {
       })
 
       // Manually set patient language and phone number for testing purposes
-      // patient.language = 'es'
-      // patient.phone = process.env.PN_TEST
+      patient.language = 'es'
+      patient.phone = process.env.PN_TEST
 
       console.log({patient})
 
@@ -83,23 +60,21 @@ router.post('/sms', sendTextAuth, async (req, res, next) => {
         To = `+1${patient.phone.replace(toRegex, '')}`
       }
     }
-
     // Print sender and message
     console.log(`Incoming text message from ${From} to ${To}: ${Body}`)
 
     // Translate message to patient language
-    // const [result, _] = await translate.translate(
-    //   Body,
-    //   patient.language || 'en'
-    // )
+    const [result, _] = await translate.translate(
+      Body,
+      patient.language || 'en'
+    )
 
-    // console.log({result})
+    console.log({result})
 
-    // Body = result
+    Body = result
 
     sendSms(Body, To)
     twiml.message(Body)
-
     res.writeHead(200, {'Content-Type': 'text/xml'})
     res.end(twiml.toString())
   } catch (error) {

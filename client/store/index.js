7a827f15a954841b761022e5929b41150b193dae
Tal Luigi <luigi.t.agr@gmail.com>
2021-07-07 23:53:34 -0400

    feat: added layout and twilio reducer export all statements in the store index file

--- client/store/index.js
+++ client/store/index.js
@@ -14,4 +14,6 @@ const store = createStore(rootReducer, middleware)
 
 // Exports
 export default store
+export * from './reducers/layoutReducer'
+export * from './reducers/twilioReducer'
 export * from './reducers/userReducer'

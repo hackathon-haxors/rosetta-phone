7d04183dddb280e08d09bbd0d863142ff8de7cb4
Tal Luigi <luigi.t.agr@gmail.com>
2021-07-25 12:38:27 -0400

    chore: added an export statement for the MadeBy component in the components index file

--- client/components/index.js
+++ client/components/index.js
@@ -4,11 +4,11 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  **/
-
 export {default as PageNotFound} from './404/PageNotFound'
 export {default as AuthForm} from './auth/AuthForm'
 export {default as Dashboard} from './dashboard/Dashboard'
 export {default as Home} from './dashboard/Home'
+export {default as MadeBy} from './dashboard/MadeBy'
 export {default as Links} from './links/Links'
 export {default as LinksBurger} from './links/LinksBurger'
 export {default as Hello} from './navbar/Hello'
